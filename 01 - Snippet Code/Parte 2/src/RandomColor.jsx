a753f992401a4fd1966dd459d9a517e768e1f5b9
Facundo Pucheta <facundopucheta94@gmail.com>
2023-08-01 19:39:14 -0300

    Exercise nº2 done - Styling while vibing

--- 01 - Snippet Code/Parte 2/src/RandomColor.jsx
+++ 01 - Snippet Code/Parte 2/src/RandomColor.jsx
@@ -1,2 +1,32 @@
-// AQUÍ VA TU COMPONENTE
-// ¡No olvides exportarlo!
+// RandomColor.jsx
+import React from 'react';
+import './RandomColor.css';
+
+const RandomColor = () => {
+  const randomColor = () => {
+    // Generar valores aleatorios para los componentes RGB (rojo, verde y azul)
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+  };
+
+  const handleButtonClick = () => {
+    // Cambiar el color de fondo de la página principal al hacer clic en el botón
+    document.body.style.backgroundColor = randomColor();
+  };
+
+  return (
+
+    <div className="random-color">
+        <section>
+        <p>Henry's | Change Color App</p>
+        <br/>
+      <button onClick={handleButtonClick}>Change the color</button>
+      </section>
+    </div>
+  );
+};
+
+export default RandomColor;
